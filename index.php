db4c17d4415ddd9ad08e856ad9e71052b7ca6f26
Guillaume Batier <guillaume.batier@gmail.com>
2018-10-18 11:54:10 +0200

    update sidebar

--- index.php
+++ index.php
@@ -63,37 +63,13 @@
 
           <!-- Categories Widget -->
           <div class="card my-4">
-            <h5 class="card-header">Categories</h5>
-            <div class="card-body">
-              <div class="row">
-                <div class="col-lg-6">
-                  <ul class="list-unstyled mb-0">
-                    <li>
-                      <a href="#">Web Design</a>
-                    </li>
-                    <li>
-                      <a href="#">HTML</a>
-                    </li>
-                    <li>
-                      <a href="#">Freebies</a>
-                    </li>
-                  </ul>
-                </div>
-                <div class="col-lg-6">
-                  <ul class="list-unstyled mb-0">
-                    <li>
-                      <a href="#">JavaScript</a>
-                    </li>
-                    <li>
-                      <a href="#">CSS</a>
-                    </li>
-                    <li>
-                      <a href="#">Tutorials</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+          <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+            <ul id="sidebar">
+                <?php dynamic_sidebar( 'sidebar-1' ); ?>
+            </ul>
+        <?php endif; ?>
+            
+
           </div>
 
           <!-- Side Widget -->
