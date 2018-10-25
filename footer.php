76571054962ec1c7c2402ec28a962818f3adfccb
Guillaume Batier <guillaume.batier@gmail.com>
2018-10-25 09:30:31 +0200

    fix footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 <footer class="py-5 bg-dark">
-    <div class="container">
+    <div class="">
         <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
     </div>
     <!-- /.container -->
--- index.php
+++ index.php
@@ -64,6 +64,7 @@
       </div>
       <!-- /.row -->
 
+      </div>
     </div>
     <!-- /.container -->
 <?php get_footer(); ?>
